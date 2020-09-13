f1344218a5a1f7ddbe017552f5e38ca30e8aea25
doofus-01 <dk0045@comcast.net>
2020-09-13 15:13:30 -0700

    fix lua error related to units.modify

--- lua/equipment_write.lua
+++ lua/equipment_write.lua
@@ -245,7 +245,9 @@ bmr_equipment.remove = function(unit_id, gear_id)
 	  else
 	      hack_u.hitpoints = hack_HP_fix_pre
 	  end
-	  helper.modify_unit({ id = unit_id }, { hitpoints = hack_u.hitpoints })
+	  bmr_helper.modify_unit({ id = unit_id }, { hitpoints = hack_u.hitpoints })
+-- this does not work	  
+--          wesnoth.units.modify({ id = unit_id }, { hitpoints = hack_u.hitpoints })
       else
           wesnoth.message(string.format("%s does not posses %s", unit_id, gear_id))      
       end
